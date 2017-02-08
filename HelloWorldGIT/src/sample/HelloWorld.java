cdfd25c2d01f3d9741e83030ad1345d25b2a3079
wangwei77721 <wayne.wang@activenetwork.com>
2017-02-08 15:16:17 +0800

    new version

--- HelloWorldGIT/src/sample/HelloWorld.java
+++ HelloWorldGIT/src/sample/HelloWorld.java
@@ -4,7 +4,7 @@ package sample;
 public class HelloWorld {
 
   public static void main( String[] args ) {
-    System.out.println("hello world");
+    System.out.println("hello world new version");
   }
 
 }
